8eb2746dfdca8367e0367087fd6b45f9d35592fc
SebastienBinet <sebastien.binet.dev@gmail.com>
2024-01-03 21:48:09 -0500

    Now generate csv file with current busses positions, import the csv in unreal (autot re-import) and display the first bus position (in TestBus actor)

--- Transit_3D_DataCrunchingMachine/Transit_3D_ConvertDataToUnrealFormat/transit_3d_convert_current_bus_position_lib.py
+++ Transit_3D_DataCrunchingMachine/Transit_3D_ConvertDataToUnrealFormat/transit_3d_convert_current_bus_position_lib.py
@@ -15,6 +15,9 @@ def get_folder_path_for_partially_processed_opendata():
 def get_busses_info_raw_csv_file_path():
     return get_folder_path_for_raw_opendata() / 'Busses_current_position.csv'
 
+def get_unreal_xyz_busses_info_csv_file_path():
+    return 'C:/GitHub/Transit_3D/Transit_3D_UnrealProject/Content/CsvOfCurrentBussesPositionsToDisplay/DT_CurrentBusInfo.csv'
+
 def read_geo_bus_positions_in_csv_file(busses_info_raw_csv_file_path):
     """Returns a list of dict"""
     ret_geo_busses_info_as_list_of_dict = []
@@ -56,17 +59,42 @@ def unrealize_zeroised_geo(zeroised_geo_busses_info_as_list_of_dict):
     for zeroised in zeroised_geo_busses_info_as_list_of_dict:
         unreal_xyz_bus_info = {
             'routeTag': zeroised['routeTag'],
-            'headingInDegreeFromNort': zeroised['heading'],
+            'headingInDegreeFromNorth': zeroised['heading'],
             'speedInMeterPerMinute': zeroised['speedKmHr'] * 60 / 1000,
-            'x': zeroised['lon'] * 111/139,
-            'y': zeroised['lat'] * 111/139,
+            'x': zeroised['lon'] * 111139,
+            'y': zeroised['lat'] * 111139,
             'z': zeroised['minutes_from_app_started'] / 60,
         }
         ret_unreal_xyz_buses_info_as_list_of_dict.append(unreal_xyz_bus_info)
     print(f'unreal_xyz_bus_info: {ret_unreal_xyz_buses_info_as_list_of_dict}')
     return ret_unreal_xyz_buses_info_as_list_of_dict
 
+def write_xyz_format_buses_info_in_csv_file(busses_info_xyz_csv_file_path, unreal_xyz_busses_info_as_list_of_dict):
+    print('33333333333333333333333333333333')
+    print(unreal_xyz_busses_info_as_list_of_dict)
 
+    result_multiline_string = ','.join(['index', 'routeTag', 'headingInDegreeFromNorth', 'speedInMeterPerMinute', '           x', '           y', '           z'])
+    for index, bus_info in enumerate(sorted(unreal_xyz_busses_info_as_list_of_dict, key=lambda x: x['routeTag'])):
+        routeTag = bus_info['routeTag']
+        headingInDegreeFromNorth = bus_info['headingInDegreeFromNorth']
+        speedInMeterPerMinute = bus_info['speedInMeterPerMinute']
+        x = bus_info['x']
+        y = bus_info['y']
+        z = bus_info['z']
+        bus_info_line_string = ','.join([
+            str(f'{index:>5}'),
+            str(f'{routeTag:>8}'),
+            str(f'{headingInDegreeFromNorth:>24}'),
+            str(f'{speedInMeterPerMinute:>21.1f}'),
+            str(f'{x:>+12.2f}'),
+            str(f'{y:>+12.2f}'),
+            str(f'{z:>+12.2f}'),
+        ])
+        result_multiline_string = '\n'.join([result_multiline_string, bus_info_line_string])
+    print('44444444444444444444444444444444')
+    print(result_multiline_string)
+    with open(busses_info_xyz_csv_file_path, 'w') as fid:
+        fid.write(result_multiline_string)
 
 def convert(app_start_lat_lon_epoch):
     busses_info_raw_csv_file_path = get_busses_info_raw_csv_file_path()
@@ -76,7 +104,9 @@ def convert(app_start_lat_lon_epoch):
     print('446')
     zeroised_geo_busses_info_as_list_of_dict = zeroise_geo(geo_busses_info_as_list_of_dict, epoch_of_rest_response, app_start_lat_lon_epoch)
     print('447')
-    unreal_xyz_buses_info_as_list_of_dict = unrealize_zeroised_geo(zeroised_geo_busses_info_as_list_of_dict)
+    unreal_xyz_busses_info_as_list_of_dict = unrealize_zeroised_geo(zeroised_geo_busses_info_as_list_of_dict)
     print('448')
-    print(unreal_xyz_buses_info_as_list_of_dict)
+    print(unreal_xyz_busses_info_as_list_of_dict)
     print('449')
+    unreal_xyz_busses_info_csv_file_path = get_unreal_xyz_busses_info_csv_file_path()
+    write_xyz_format_buses_info_in_csv_file(unreal_xyz_busses_info_csv_file_path, unreal_xyz_busses_info_as_list_of_dict)
